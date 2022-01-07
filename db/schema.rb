84994390508fda357a03cb64781008d84b5c9d39
Juan Vargas <itwarp_jvargas@iseatz.com>
2022-01-07 16:37:22 -0300

    Edit README

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_17_152632) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "currencies", force: :cascade do |t|
     t.string "currency_id"
     t.string "name"
